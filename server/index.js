b5e438d8633fec97cf237e96cd1ab275806590cb
Curtis Timson <curt@live.co.uk>
2019-04-01 14:57:24 +0100

    Return hello world

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import Html from './htmlHelper';
 
 const app = express();
-// const apiRoutes = express.Router();
-
 const port = process.env.PORT || 8080;
 
 console.log('App started');
+
+app.get('/', (req, res) => {
+
+    res.send(
+        Html({
+            body: 'hello world',
+            title: 'hello world',
+        })
+    );
+});
 
 app.listen(port);
 console.log(`App listening on port ${port}`);
